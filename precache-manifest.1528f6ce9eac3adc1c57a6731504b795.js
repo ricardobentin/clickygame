28ba6cfd6c20f2e6bfac34c2b564f2ed087a6081
Nano Bacano <ricardo.bentin@gmail.com>
2018-10-18 19:40:14 -0400

    Updates

---
+++ precache-manifest.1528f6ce9eac3adc1c57a6731504b795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a6561277f64e584ac77695fac200d69",
+    "url": "/clickygame/static/media/header.6a656127.jpg"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "44d74237642dde555d7b",
+    "url": "/clickygame/static/js/main.44d74237.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/clickygame/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "44d74237642dde555d7b",
+    "url": "/clickygame/static/css/main.224cf87c.chunk.css"
+  },
+  {
+    "revision": "5006d703f3838ef600546d9a03c4ddb5",
+    "url": "/clickygame/index.html"
+  }
+];
